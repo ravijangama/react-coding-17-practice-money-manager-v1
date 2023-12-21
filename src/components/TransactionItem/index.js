a89b2b739e289a82e0b5710adf2639ea07dc3f2e
RAVI JANGAMA <149039551+ravijangama@users.noreply.github.com>
2023-12-22 00:48:27 +0530

    Update index.js

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,1 +1,21 @@
 // Write your code here
+import './index.css'
+
+const TransactionItem = props => {
+  const {transactionDetails} = props
+  const {titleInput, amountInput, optionType} = transactionDetails
+  return (
+    <li className="transaction-container">
+      <p className="title-p">{titleInput}</p>
+      <p className="title-p">{amountInput}</p>
+      <p className="title-p">{optionType}</p>
+      <button type="button" className="title-p" data-testid="delete">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+          alt="delete"
+        />
+      </button>
+    </li>
+  )
+}
+export default TransactionItem

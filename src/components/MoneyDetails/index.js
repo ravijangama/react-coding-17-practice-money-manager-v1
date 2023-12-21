ede5b095e0c412b76e437f13ae5992a562829571
RAVI JANGAMA <149039551+ravijangama@users.noreply.github.com>
2023-12-22 00:49:26 +0530

    Update index.js

--- src/components/MoneyDetails/index.js
+++ src/components/MoneyDetails/index.js
@@ -1,17 +1,39 @@
 // Write your code here
 import './index.css'
 
-const MoneyDetails = props => {
-  const {imgUrl, amountType, altText, bgName} = props
-  const bgClassNames = `money-details-container ${bgName}`
-  return (
-    <li className={bgClassNames}>
-      <img src={imgUrl} alt={altText} className="type-img" />
+const BALANCE_URL =
+  'https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png '
+
+const INCOME_URL =
+  'https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png'
+
+const EXPENSE_URL =
+  'https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png '
+
+const MoneyDetails = () => (
+  <ul className="money-detail-ul">
+    <li className="money-details-container bg-balance ">
+      <img src={BALANCE_URL} alt="balance" className="type-img" />
       <div className="type-container">
-        <p>Your {amountType}</p>
-        <p>Rs 0</p>
+        <p>Your Balance</p>
+        <p data-testid="balanceAmount">Rs 0</p>
       </div>
     </li>
-  )
-}
+    <li className="money-details-container bg-income">
+      <img src={INCOME_URL} alt="income" className="type-img" />
+      <div className="type-container">
+        <p>Your Income</p>
+        <p data-testid="incomeAmount">Rs 0</p>
+      </div>
+    </li>
+    <li className="money-details-container bg-expense">
+      <img src={EXPENSE_URL} alt="expenses" className="type-img" />
+      <div className="type-container">
+        <p>Your Expenses</p>
+        <p data-testid="expensesAmount">Rs 0</p>
+      </div>
+    </li>
+  </ul>
+)
+
 export default MoneyDetails

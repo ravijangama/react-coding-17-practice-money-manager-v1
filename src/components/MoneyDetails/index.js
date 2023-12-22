497597952134ca473ec907313dc227c080a246d1
RAVI JANGAMA <149039551+ravijangama@users.noreply.github.com>
2023-12-22 17:43:41 +0530

    Update index.js

--- src/components/MoneyDetails/index.js
+++ src/components/MoneyDetails/index.js
@@ -10,30 +10,34 @@ const INCOME_URL =
 const EXPENSE_URL =
   'https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png '
 
-const MoneyDetails = () => (
-  <ul className="money-detail-ul">
-    <li className="money-details-container bg-balance ">
-      <img src={BALANCE_URL} alt="balance" className="type-img" />
-      <div className="type-container">
-        <p>Your Balance</p>
-        <p data-testid="balanceAmount">Rs 0</p>
-      </div>
-    </li>
-    <li className="money-details-container bg-income">
-      <img src={INCOME_URL} alt="income" className="type-img" />
-      <div className="type-container">
-        <p>Your Income</p>
-        <p data-testid="incomeAmount">Rs 0</p>
-      </div>
-    </li>
-    <li className="money-details-container bg-expense">
-      <img src={EXPENSE_URL} alt="expenses" className="type-img" />
-      <div className="type-container">
-        <p>Your Expenses</p>
-        <p data-testid="expensesAmount">Rs 0</p>
-      </div>
-    </li>
-  </ul>
-)
+const MoneyDetails = props => {
+  const {stateObj} = props
+  const {balanceAmount, incomeAmount, expensesAmount} = stateObj
+  return (
+    <ul className="money-detail-ul">
+      <li className="money-details-container bg-balance ">
+        <img src={BALANCE_URL} alt="balance" className="type-img" />
+        <div className="type-container">
+          <p>Your Balance</p>
+          <p data-testid="balanceAmount">Rs {balanceAmount}</p>
+        </div>
+      </li>
+      <li className="money-details-container bg-income">
+        <img src={INCOME_URL} alt="income" className="type-img" />
+        <div className="type-container">
+          <p>Your Income</p>
+          <p data-testid="incomeAmount">Rs {incomeAmount}</p>
+        </div>
+      </li>
+      <li className="money-details-container bg-expense">
+        <img src={EXPENSE_URL} alt="expenses" className="type-img" />
+        <div className="type-container">
+          <p>Your Expenses</p>
+          <p data-testid="expensesAmount">Rs {expensesAmount}</p>
+        </div>
+      </li>
+    </ul>
+  )
+}
 
 export default MoneyDetails
